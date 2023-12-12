78557a1e6e407d10e119cb4eaec49fc2a204dfb1
MarkNicholas-cpp <marknicholas2002112@gmail.com>
2023-12-12 15:19:22 +0530

    Started....

--- HandShake-Frontend/src/app/app-routing.module.ts
+++ HandShake-Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'chats',pathMatch:'full'},
+  {path:'chats'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
